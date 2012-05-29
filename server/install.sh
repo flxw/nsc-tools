a52c731712fcc751e2f5d66d2902d6be00d3e201
n0stradamus <send_me_a_message_on_github@ignorethis.com>
2012-05-29 16:56:35 +0200

    Fix stupid installation bug - this time on server side

--- server/install.sh
+++ server/install.sh
@@ -18,4 +18,4 @@ echo "Linking pkg management scripts to $REPO_USER's home..."
 /bin/ln "$PWD/update-pkgs"   ~/ 
 
 echo "Creating symlink in $REPO_USER's home to $REPO_DIR..."
-/bin/ln -s "REPO_DIR/nsc" ~/nsc_dir_link
+/bin/ln -s "$REPO_DIR/nsc" ~/nsc_dir_link
